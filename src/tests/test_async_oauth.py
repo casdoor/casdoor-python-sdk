7db8b78dd8a36e4407fc641b543fe1f0d9af993c
jump2cn <jump2cn@gmail.com>
2023-04-24 08:53:56 +0800

    to: fix ci lint check

--- src/tests/test_async_oauth.py
+++ src/tests/test_async_oauth.py
@@ -148,9 +148,9 @@ class TestOAuth(IsolatedAsyncioTestCase):
 
     def mocked_enforce_requests_post(*args, **kwargs):
         class MockResponse:
-            def __init__(self, 
-                         json_data, 
-                         status_code=200, 
+            def __init__(self,
+                         json_data,
+                         status_code=200,
                          headers={'content-type': 'json'}):
                 self.json_data = json_data
                 self.status_code = status_code
